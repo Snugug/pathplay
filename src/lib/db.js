4f0fe6fc3605a611eb1c0b6df1b0ac548579915a
Sam Richard <snugug@google.com>
2023-04-15 19:11:24 -0400

    Update DB to use Dexie and workers

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,72 +1,11 @@
-import { openDB } from 'idb';
+import { wrap } from 'comlink';
 
-/**
- * Creates a database
- */
-async function database() {
-  const db = await openDB('pathplay', 1, {
-    upgrade(db, oldV, newV, tx) {
-      switch (oldV) {
-        case 0: {
-          // Characters Store
-          const characters = db.createObjectStore('characters', {
-            keyPath: 'id',
-            autoIncrement: true,
-          });
-          characters.createIndex('name', 'name', { unique: false });
-          characters.createIndex('level', 'level', { unique: false });
-          characters.createIndex('created', 'created', { unique: false });
-          characters.createIndex('updated', 'updated', { unique: false });
+export const database = wrap(
+  new Worker(new URL('$lib/workers/db.js?url', import.meta.url), {
+    type: 'module',
+  }),
+);
 
-          // Feats
-          const feats = db.createObjectStore('feats', {
-            keyPath: 'id',
-            autoIncrement: true,
-          });
-          feats.createIndex('name', 'name', { unique: true });
-          feats.createIndex('level', 'level', { unique: false });
+export const db = database.db;
 
-          // Spells
-          const spells = db.createObjectStore('spells', {
-            keyPath: 'id',
-            autoIncrement: true,
-          });
-          spells.createIndex('name', 'name', { unique: true });
-          spells.createIndex('level', 'level', { unique: false });
-
-          // Classes
-          const classes = db.createObjectStore('classes', {
-            keyPath: 'id',
-            autoIncrement: true,
-          });
-          classes.createIndex('name', 'name', { unique: true });
-
-          // Tags
-          const tags = db.createObjectStore('tags', {
-            keyPath: 'id',
-            autoIncrement: true,
-          });
-          tags.createIndex('name', 'name', { unique: true });
-
-          // Equipment
-          const equipment = db.createObjectStore('equipment', {
-            keyPath: 'id',
-            autoIncrement: true,
-          });
-          equipment.createIndex('name', 'name', { unique: true });
-          equipment.createIndex('price', 'price', { unique: false });
-
-          // Portraits
-          db.createObjectStore('portraits', {
-            keyPath: 'id',
-            autoIncrement: true,
-          });
-        }
-      }
-    },
-  });
-
-  return db;
-}
-
-export const db = await database();
+export const fetchAndUpdateData = database.fetchAndUpdateData;
---
+++ src/lib/workers/db.js
@@ -0,0 +1,30 @@
+import { expose } from 'comlink';
+import Dexie from 'dexie';
+
+const db = new Dexie('pathplay');
+
+db.version(1).stores({
+  data: 'url, name, slug, type, id',
+  meta: 'type',
+  // Add characters
+});
+
+/**
+ *
+ * @param {Object[]} data
+ */
+async function fetchAndUpdateData(data) {
+  const files = (
+    await Promise.all(
+      data.map((d) => fetch(`/${d.path}`).then((res) => res.json())),
+    )
+  ).flat();
+
+  await db.data.bulkPut(files);
+  await db.meta.bulkPut(data);
+}
+
+expose({
+  db,
+  fetchAndUpdateData,
+});
