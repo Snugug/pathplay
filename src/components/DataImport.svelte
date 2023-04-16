88c6ec0b06445efb1f781e240da459098a93b98c
Sam Richard <snugug@google.com>
2023-04-16 06:38:52 -0400

    Fix async filter, add button to trigger load

--- src/components/DataImport.svelte
+++ src/components/DataImport.svelte
@@ -1,15 +1,19 @@
 <script>
   import { onMount } from 'svelte';
+  import { db, fetchAndUpdateData } from '$database';
 
   let size;
+  let needsUpdate = false;
+  let dataToUpdate = [];
 
   onMount(async () => {
-    const { db, fetchAndUpdateData } = await import('$database');
-
     const { data } = await (await fetch('/data/meta.json')).json();
 
-    const dataToUpdate = data.filter(async (d) => {
-      const current = await db.data.get(d.type);
+    const meta = await db.meta.toArray();
+
+    dataToUpdate = data.filter((d) => {
+      const current = meta.find((m) => m.type === d.type);
+
       if (current?.hash !== d.hash) {
         return true;
       }
@@ -20,8 +24,29 @@
       dataToUpdate.map((d) => d.size).reduce((a, b) => a + b, 0) / 1024,
     );
 
-    await fetchAndUpdateData(dataToUpdate);
+    if (dataToUpdate.length) {
+      needsUpdate = true;
+    }
   });
 </script>
 
-<h1>Total data size: {size}Kb</h1>
+{#if needsUpdate}
+  <button
+    on:click|preventDefault={async () =>
+      await fetchAndUpdateData(dataToUpdate).then(() => (needsUpdate = false))}
+    >Update data? {size}Kb</button
+  >
+{/if}
+
+<style>
+  button {
+    position: fixed;
+    bottom: 0;
+    right: 0;
+    background: #ffffff;
+    border: 1px solid #000000;
+    padding: 0.5rem;
+    font-size: 1rem;
+    color: #000000;
+  }
+</style>
