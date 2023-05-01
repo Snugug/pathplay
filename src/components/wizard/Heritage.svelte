55692520a0b12ec69e5053aff9a90ed2a673f374
Sam Richard <snugug@google.com>
2023-05-01 19:42:16 -0400

    Add heritage wizard

---
+++ src/components/wizard/Heritage.svelte
@@ -0,0 +1,223 @@
+<script>
+  import { getContext, setContext } from 'svelte';
+  import { writable } from 'svelte/store';
+  import ListFilter from '$components/ListFilter.svelte';
+  import ListPreview from '$components/ListPreview.svelte';
+  import Ancestry from '$components/Ancestry.svelte';
+  import Expand from '$icons/expand_less.svg?raw';
+  import { filterCollection } from '$database';
+
+  // Get the character context
+  const character = getContext('character');
+  const choices = getContext('choices');
+
+  // Set up the search context
+  const source = writable([]);
+  const results = writable([]);
+
+  let dialog;
+  let preview;
+  let details;
+
+  const open = true;
+
+  setContext('filter', {
+    source,
+    results,
+    placeholder: 'Filter heritages...',
+  });
+
+  $: {
+    const heritages = $choices.ancestry?.heritages || [];
+    // Get ancestries
+    filterCollection('data', [{ key: 'url', value: heritages, operator: 'in' }])
+      .then((her) => {
+        if (her) {
+          return her
+            .sort((a, b) => a.name.localeCompare(b.name))
+            .reduce(
+              (acc, cur) => {
+                if (cur.rarity === 'common') {
+                  acc[0].push(cur);
+                } else if (cur.rarity === 'uncommon') {
+                  acc[1].push(cur);
+                } else if (cur.rarity === 'rare') {
+                  acc[2].push(cur);
+                }
+                return acc;
+              },
+              [[], [], []],
+            );
+        }
+        return her;
+      })
+      .then((her) => {
+        console.log(her);
+        if (her.length > 0) {
+          source.set(her.flat());
+          results.set(her.flat());
+        }
+      });
+
+    //   .flat(),
+
+    // .then((anc) => {
+    //   source.set(anc);
+    //   results.set(anc);
+    // });
+  }
+
+  /**
+   * Preview an ancestry
+   * @param {string} a The ancestry to preview
+   * @return {function} A function to preview the ancestry
+   */
+  function previewAncestry(a) {
+    return () => {
+      preview = a;
+      // dialog.showModal();
+    };
+  }
+
+  /**
+   * Select an ancestry
+   */
+  function select() {
+    choices.set(Object.assign($choices, { heritage: preview }));
+    character.set(
+      Object.assign($character, {
+        heritage: {
+          name: preview.name,
+          id: preview.url,
+        },
+      }),
+    );
+    details.removeAttribute('open');
+    details.focus();
+  }
+</script>
+
+<details open bind:this={details}>
+  <summary><span class="position">{@html Expand}</span>Heritage</summary>
+  <div class="wizard">
+    <div class="filter">
+      <ListFilter />
+    </div>
+
+    {#if $results?.length > 0}
+      <ul class="ancestries">
+        {#each $results as item}
+          <li>
+            <button class="preview" on:click={previewAncestry(item)}>
+              <ListPreview
+                name={item.name}
+                source={item?.source.length > 0 ? item.source[0].name : ''}
+                rarity={item.rarity}
+                image={item?.images ? item.images[0] : {}}
+              />
+            </button>
+          </li>
+        {/each}
+      </ul>
+    {/if}
+
+    <dialog bind:this={dialog} open={open ? true : null}>
+      {#if preview}
+        <Ancestry ancestry={preview} />
+
+        <form class="dialog-options" method="dialog">
+          <button on:click|preventDefault={select}>Choose {preview.name}</button
+          >
+          <button
+            class="dialog-options--cancel"
+            value="cancel"
+            formmethod="dialog">Cancel</button
+          >
+        </form>
+      {/if}
+    </dialog>
+  </div>
+</details>
+
+<style lang="scss">
+  details {
+    padding: 1rem 0;
+    border-bottom: 1px solid var(--theme-text);
+  }
+  summary {
+    font-size: 2rem;
+    cursor: pointer;
+  }
+
+  dialog[open] {
+    position: sticky;
+    top: 1rem;
+    align-self: flex-start;
+    margin: 0;
+    width: 100%;
+    background-color: transparent;
+    border: none;
+    padding: 0;
+    color: var(--theme-text);
+    max-height: calc(100vh - 2rem);
+    overflow-y: auto;
+  }
+
+  .position {
+    display: inline-block;
+    fill: var(--theme-text);
+    margin-inline-end: 0.5rem;
+    transition: transform 0.2s ease-in-out;
+    transform: rotate(90deg);
+
+    details[open] & {
+      transform: rotate(180deg);
+    }
+  }
+
+  // summary:focus {
+  //   outline: 1px solid var(--theme-text);
+  // }
+
+  .filter {
+    grid-column: 1 / -1;
+  }
+
+  .wizard {
+    display: grid;
+    grid-template-columns: 400px 1fr;
+    row-gap: 1rem;
+    column-gap: 2rem;
+    padding: 1rem;
+  }
+
+  .ancestries {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  }
+
+  .preview {
+    background: none;
+    border: none;
+    width: 100%;
+    padding: 0;
+  }
+
+  .dialog-options {
+    max-width: calc(65ch + 200px + 2rem);
+    margin-inline: auto;
+    margin-top: 1rem;
+    color: var(--black);
+    display: flex;
+    flex-direction: row-reverse;
+    gap: 1rem;
+
+    &--cancel {
+      color: var(--theme-text);
+      border: 0;
+      background: none;
+      text-decoration: underline;
+    }
+  }
+</style>
--- src/routes/EditCharacter.svelte
+++ src/routes/EditCharacter.svelte
@@ -2,6 +2,7 @@
   import { db } from '$database';
   import { structure } from '$lib/characters';
   import AncestryWizard from '$components/wizard/Ancestry.svelte';
+  import HeritageWizard from '$components/wizard/Heritage.svelte';
   import Missing from '$icons/cancel.svg?raw';
   import Done from '$icons/check_circle.svg?raw';
   import { writable } from 'svelte/store';
@@ -16,18 +17,31 @@
 
   const character = writable(null);
 
+  const choices = writable({
+    ancestry: null,
+    heritage: null,
+    background: null,
+    class: null,
+    abilities: null,
+    skills: null,
+    feats: null,
+  });
+
   // It now works for both new character and edit character!
   if (params?.id) {
-    console.log('Getting existing character');
     db.characters.get(Number(params.id)).then((a) => {
       if (!a) {
         push('/characters/new');
         return;
       }
+      if (a.ancestry.id) {
+        db.data.get(a.ancestry.id).then((b) => {
+          choices.set(Object.assign($choices, { ancestry: b }));
+        });
+      }
       character.set(a);
     });
   } else {
-    console.log('creating new character');
     const today = new Date();
     character.set(
       Object.assign(structuredClone(structure), {
@@ -41,22 +55,15 @@
     });
   }
 
-  const choices = writable({
-    ancestry: null,
-    heritage: null,
-    background: null,
-    class: null,
-    abilities: null,
-    skills: null,
-    feats: null,
-  });
-
   setContext('character', character);
   setContext('choices', choices);
 
   $: {
+    console.log($choices);
+  }
+
+  $: {
     if ($character) {
-      console.log($character);
       db.characters.update($character.id, $character);
     }
   }
@@ -66,6 +73,7 @@
   <h1 class="meta--title">Character Builder</h1>
   {#if $character}
     <input
+      class="meta--name"
       type="text"
       aria-label="Character Name"
       placeholder="Character Name"
@@ -74,12 +82,19 @@
     <div class="meta--steps">
       {#each Object.entries($choices) as [step, value]}
         <div class="meta--step">
-          {#if value?.name}
-            {@html Done}
+          <h2>{step.charAt(0).toUpperCase() + step.slice(1)}</h2>
+          {#if value}
+            {#if value.name}
+              <p class="meta--name">{value.name}</p>
+            {:else}
+              <div class="meta--done" aria-label="Done">{@html Done}</div>
+            {/if}
           {:else}
-            {@html Missing}
+            <div class="meta--missing" aria-label="Missing">
+              {@html Missing}
+            </div>
           {/if}
-          <h2>{step}</h2>
+
           <!-- <p>{JSON.stringify($choices[step])}</p> -->
         </div>
       {/each}
@@ -88,14 +103,55 @@
 </section>
 
 <AncestryWizard />
+<HeritageWizard />
 
 <style lang="scss">
   .meta {
     border: 1px solid currentColor;
     padding: 0.5rem;
+    display: grid;
+    grid-template-columns: 200px 1fr;
+    gap: 1rem;
+    container-type: inline-size;
+    container-name: meta;
+
+    &--name {
+      grid-column: 1;
+    }
+
+    &--steps {
+      display: grid;
+      grid-template-columns: repeat(auto-fit, minmax(10ch, 1fr));
+      align-items: flex-start;
+      justify-content: center;
+
+      gap: 1rem;
+
+      grid-row: 3;
+      grid-column: 1 / -1;
+
+      @container meta (min-width: 635px) {
+        grid-row: 1 / span 3;
+        grid-column: 2;
+      }
+    }
+
+    &--step {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      gap: 0.25rem;
+    }
 
     &--title {
       font-size: 1.25rem;
+    }
+
+    &--done {
+      fill: var(--theme-green);
+    }
+    &--missing {
+      fill: var(--theme-red);
     }
   }
 </style>
