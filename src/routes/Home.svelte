e9d8d9707d8e4fd9002b78728fa324f037bbfae4
Sam Richard <snugug@google.com>
2023-05-04 07:47:02 -0400

    Add filter and new character button

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,12 +1,22 @@
 <script lang="ts">
   import { db } from '$database';
   import CharacterCard from '$components/CharacterCard.svelte';
+  import ListFilter from '$components/ListFilter.svelte';
   import { writable } from 'svelte/store';
+  import { setContext } from 'svelte';
 
   const characters = writable(null);
+  const results = writable([]);
 
   db.characters.toArray().then((c) => {
     characters.set(c);
+    results.set(c);
+  });
+
+  setContext('filter', {
+    source: characters,
+    results,
+    placeholder: 'Find character...',
   });
 
   /**
@@ -21,6 +31,10 @@
       const index = $characters.findIndex((c) => c.id === id);
       $characters.splice(index, 1);
       characters.set($characters);
+
+      const resultsIndex = $results.findIndex((c) => c.id === id);
+      $results.splice(resultsIndex, 1);
+      results.set($results);
     } catch (e) {
       console.error(e);
     }
@@ -29,11 +43,19 @@
   }
 </script>
 
-<div>
-  {#if $characters}
-    <ul class="characters">
-      {#each $characters as character}
-        <li>
+<div class="home">
+  <header class="home--header">
+    <h2 class="type--h2">My Characters</h2>
+    <div class="home--actions">
+      <a href="/characters/new" class="home--add">+ New Character</a>
+      <ListFilter />
+    </div>
+  </header>
+
+  {#if $results}
+    <ul class="home--characters">
+      {#each $results as character}
+        <li class="home--character">
           <CharacterCard {character} on:deleteCharacter={handleDelete} />
         </li>
       {/each}
@@ -44,33 +66,64 @@
 </div>
 
 <style lang="scss">
-  .characters {
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(min(100%, 300px), 1fr));
-    gap: 2rem;
+  .home {
+    &--header {
+      border-bottom: 1px solid var(--theme-text);
+      padding-bottom: 1.5rem;
+      margin-bottom: 1.5rem;
+    }
 
-    li {
+    &--actions {
       display: flex;
-      flex-direction: column;
+      flex-wrap: wrap;
+      gap: 1rem;
+    }
+
+    &--add {
+      color: var(--black);
+      background-color: var(--theme-text);
+      border-radius: 3px;
+      border: 1px solid var(--black);
+      padding: 0.25rem 0.5rem;
+      text-decoration: none;
+      display: flex;
+      align-items: center;
       justify-content: center;
     }
-  }
 
-  .add {
-    aspect-ratio: 1 / 1;
-    border: 5px solid var(--theme-text);
-    border-radius: 5px;
-    width: 80%;
-    width: 4rem;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    align-self: center;
+    &--characters {
+      display: grid;
+      grid-template-columns: repeat(auto-fill, minmax(min(100%, 300px), 1fr));
+      gap: 2rem;
 
-    :global(svg) {
-      fill: var(--theme-text);
-      height: 80%;
-      width: 80%;
+      li {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+      }
+    }
+
+    &--character {
+      display: flex;
+      align-items: center;
     }
   }
+
+  // .add {
+  //   aspect-ratio: 1 / 1;
+  //   border: 5px solid var(--theme-text);
+  //   border-radius: 5px;
+  //   width: 80%;
+  //   width: 4rem;
+  //   display: flex;
+  //   justify-content: center;
+  //   align-items: center;
+  //   align-self: center;
+
+  //   :global(svg) {
+  //     fill: var(--theme-text);
+  //     height: 80%;
+  //     width: 80%;
+  //   }
+  // }
 </style>
