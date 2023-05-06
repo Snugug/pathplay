74e7160cf7039d52813db99230b0b954773748d0
Sam Richard <snugug@google.com>
2023-05-06 07:43:47 -0400

    Make details reusable

---
+++ src/components/Details.svelte
@@ -0,0 +1,58 @@
+<script>
+  import Expand from '$icons/expand_less.svg?raw';
+  export let title = '';
+
+  let details;
+
+  /**
+   * Close the details element
+   */
+  export function close() {
+    details.removeAttribute('open');
+    details.focus();
+  }
+
+  /**
+   * Open the details element
+   */
+  export function open() {
+    details.setAttribute('open', '');
+    details.focus();
+  }
+</script>
+
+<details bind:this={details}>
+  <summary
+    ><span class="position">{@html Expand}</span><span class="summary--title"
+      >{title}</span
+    ></summary
+  >
+  <slot />
+</details>
+
+<style lang="scss">
+  details {
+    padding: 1rem 0;
+    border-bottom: 1px solid var(--theme-text);
+  }
+
+  summary {
+    cursor: pointer;
+
+    &::-webkit-details-marker {
+      display: none;
+    }
+  }
+
+  .position {
+    display: inline-block;
+    fill: var(--theme-text);
+    margin-inline-end: 0.5rem;
+    transition: transform 0.2s ease-in-out;
+    transform: rotate(90deg);
+
+    details[open] & {
+      transform: rotate(180deg);
+    }
+  }
+</style>
