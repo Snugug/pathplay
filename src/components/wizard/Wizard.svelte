fc1eed406d913c3bbf7e7210cadf8c37b381cf84
Sam Richard <snugug@google.com>
2023-05-06 07:44:05 -0400

    Make Wizard component reusable

---
+++ src/components/wizard/Wizard.svelte
@@ -0,0 +1,196 @@
+<script>
+  import Details from '$components/Details.svelte';
+  import ListFilter from '$components/ListFilter.svelte';
+  import ListPreview from '$components/ListPreview.svelte';
+  import Preview from '$components/Preview.svelte';
+  import { filterCollection } from '$database';
+
+  import { writable } from 'svelte/store';
+  import { getContext, setContext } from 'svelte';
+
+  export let title = '';
+  export let filter = [];
+
+  // Get the character context
+  const character = getContext('character');
+  const choices = getContext('choices');
+
+  // Set up the search context
+  const source = writable([]);
+  const results = writable([]);
+
+  // Set up variables
+  let dialog;
+  let preview;
+  let details;
+
+  // Set up context
+  setContext('filter', {
+    source,
+    results,
+    placeholder: `Filter ${title}...`,
+  });
+
+  // Get ancestries
+  $: {
+    if (filter.length) {
+      filterCollection('data', filter)
+        .then((anc) =>
+          anc
+            .sort((a, b) => a?.name.localeCompare(b?.name))
+            .reduce(
+              (acc, cur) => {
+                if (cur.rarity === 'common') {
+                  acc[0].push(cur);
+                } else if (cur.rarity === 'uncommon') {
+                  acc[1].push(cur);
+                } else if (cur.rarity === 'rare') {
+                  acc[2].push(cur);
+                }
+                return acc;
+              },
+              [[], [], []],
+            )
+            .flat(),
+        )
+        .then((anc) => {
+          source.set(anc);
+          results.set(anc);
+        });
+    }
+  }
+
+  /**
+   * Preview selection
+   * @param {string} a The item to preview
+   * @return {function} A function to launch the preview
+   */
+  function previewAncestry(a) {
+    return () => {
+      preview = a;
+      // dialog.showModal();
+    };
+  }
+
+  /**
+   * Select an item
+   */
+  function select() {
+    const key = title.toLocaleLowerCase();
+
+    choices.set(Object.assign($choices, { [key]: preview }));
+    character.set(
+      Object.assign($character, {
+        [key]: {
+          name: preview.name,
+          id: preview.url,
+        },
+      }),
+    );
+
+    details.close();
+  }
+</script>
+
+<Details bind:this={details} {title}>
+  <div class="wizard">
+    <div class="filter">
+      <ListFilter />
+    </div>
+
+    {#if $results?.length > 0}
+      <ul class="ancestries">
+        {#each $results as item}
+          <li>
+            <button class="preview" on:click={previewAncestry(item)}>
+              <ListPreview
+                name={item.name}
+                source={item?.source.length > 0 ? item.source[0].name : ''}
+                rarity={item.rarity}
+                image={item?.images ? item.images[0] : {}}
+              />
+            </button>
+          </li>
+        {/each}
+      </ul>
+    {/if}
+
+    <dialog bind:this={dialog} open={open ? true : null}>
+      {#if preview}
+        <Preview {preview} />
+
+        <form class="dialog-options" method="dialog">
+          <button on:click|preventDefault={select}>Choose {preview.name}</button
+          >
+          <button
+            class="dialog-options--cancel"
+            value="cancel"
+            formmethod="dialog">Cancel</button
+          >
+        </form>
+      {/if}
+    </dialog>
+  </div>
+</Details>
+
+<style lang="scss">
+  :global(.summary--title) {
+    font-size: 2rem;
+  }
+
+  dialog[open] {
+    position: sticky;
+    top: 1rem;
+    align-self: flex-start;
+    margin: 0;
+    width: 100%;
+    background-color: transparent;
+    border: none;
+    padding: 0;
+    color: var(--theme-text);
+    max-height: calc(100vh - 2rem);
+    overflow-y: auto;
+  }
+
+  .filter {
+    grid-column: 1 / -1;
+  }
+
+  .wizard {
+    display: grid;
+    grid-template-columns: 400px 1fr;
+    row-gap: 1rem;
+    column-gap: 2rem;
+    padding: 1rem;
+  }
+
+  .ancestries {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  }
+
+  .preview {
+    background: none;
+    border: none;
+    width: 100%;
+    padding: 0;
+  }
+
+  .dialog-options {
+    max-width: calc(65ch + 200px + 2rem);
+    margin-inline: auto;
+    margin-top: 1rem;
+    color: var(--black);
+    display: flex;
+    flex-direction: row-reverse;
+    gap: 1rem;
+
+    &--cancel {
+      color: var(--theme-text);
+      border: 0;
+      background: none;
+      text-decoration: underline;
+    }
+  }
+</style>
